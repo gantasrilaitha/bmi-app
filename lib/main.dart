8171306750168385fb6dfe207e12596104849996
gantasrilaitha <srilaitha2003@gmail.com>
2022-11-13 19:47:17 +0530

    dataservice Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+/*import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:firstapp/secondpage.dart';
 void main() {
@@ -214,4 +214,73 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
 
               )]));
-  }}
+  }}*/
+
+
+
+import 'package:flutter/material.dart';
+import 'package:firstapp/dataservice.dart';
+import 'package:firstapp/models.dart';
+
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _cityTextController = TextEditingController();
+  final _dataservice=DataService();
+
+
+  WeatherResponse?_response;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.lightBlueAccent,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if(_response !=null)
+                  Column(
+                    children: [
+                      Image.network(_response!.iconUrl),
+                      Text(
+                        '${_response!.tempInfo.temperature}°',
+                        style: TextStyle(fontSize: 40),
+                      ),
+                      Text(_response!.weatherInfo.description)
+                    ],
+                  ),
+
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 50),
+                  child: SizedBox(
+                    width: 150,
+                    child: TextField(
+                        controller: _cityTextController,
+                        decoration: InputDecoration(labelText: 'City'),
+                        textAlign: TextAlign.center),
+                  ),
+                ),
+                ElevatedButton(onPressed: _search, child: Text('Search'))
+              ],
+            ),
+          ),
+        ));
+  }
+
+  void _search() async {
+    final response = await _dataservice.getWeather(_cityTextController.text);
+    setState(() => _response = response);
+  }
+}
